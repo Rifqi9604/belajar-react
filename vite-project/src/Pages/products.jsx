c8d7917609210ac55e544a4ba9bcd71a51c70566
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2023-12-23 18:16:20 +0700

    heheh

--- vite-project/src/Pages/products.jsx
+++ vite-project/src/Pages/products.jsx
@@ -1,25 +1,26 @@
 import Button from "../components/Elements/Button/Buttons"
 import CardProduct from "../components/Fragments/CardProduct"
+import { useState } from "react"
 
 const products = [
     {
         id: 1, 
         title: "Sepatu Baru", 
-        price: "Rp 2.000.000",
+        price: 2000000,
         image: "/images/sepatu.jpg",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta saepe quibusdam voluptatum laudantium ex omnis vitae consequatur, pariatur adipisci inventore est fuga, at iure quia optio magnam, enim aspernatur assumenda."
     },
     {
         id: 2, 
         title: "Sepatu lama", 
-        price: "Rp 1.000.000",
+        price: 1000000,
         image: "/images/sepatu.jpg",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta saepe quibusdam voluptatum laudantium ex omnis vitae consequatur"
     },
     {
         id: 3, 
         title: "Sepatu Adadong", 
-        price: "Rp 5.000.000",
+        price: 5000000,
         image: "/images/sepatu.jpg",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta saepe quibusdam voluptatum laudantium ex omnis ."
     },
@@ -35,6 +36,25 @@ const ProductPage   = () => {
         localStorage.removeItem("password")
         window.location.href = "/login"
     }
+    
+    const [cart, setCart] = useState([
+        {
+            name: "Sepatu Lama",
+            qty: 1
+        }
+    ])
+
+    const handleAddCart = (productName) => {
+        setCart([
+            ...cart,
+            {
+                name: productName,
+                qty: 1
+            }
+        ])
+    }
+
+
     return(
         <>
             <div className="flex justify-end h-20 bg-blue-600 text-white px-10 items-center">
@@ -44,21 +64,27 @@ const ProductPage   = () => {
                 </Button>
             </div>
             <div className="flex justify-center py-5">
-                <div className="f w-2/3">
+                <div className="flex flex-wrap w-2/3">
                 {products.map(product => (
                     <CardProduct key={product.id}>
                         <CardProduct.Header image={product.image}/>
                             <CardProduct.Body title={product.title}>
                                 {product.description}
                             </CardProduct.Body>
-                        <CardProduct.Footer price={product.price} />
+                        <CardProduct.Footer price={product.price} handleAddCart={handleAddCart}/>
                         </CardProduct>
                 ))}
                 </div>
-                <div className="w-2/3 bg-red-200">
-                    tes
+                <div className="w-1/3">
+                    <div className="text-3xl font-bold text-blue-600 bg-red-200">
+                        Cart    
+                    </div>
+                    <ul>
+                        {cart.map(item => (
+                            <li key={item.name}>{item.name}</li>
+                        ))}
+                    </ul>
                 </div>
-
             </div>
         </>
     )
--- vite-project/src/components/Fragments/CardProduct.jsx
+++ vite-project/src/components/Fragments/CardProduct.jsx
@@ -2,7 +2,7 @@ import Button from "../Elements/Button/Buttons"
 const CardProduct = (props) => {
     const { children } = props
     return (
-        <div className="w-full max-w-sm bg-gray-800 border border-gray-700 shadow mx-2 rounded-lg flex flex-col justify-between">
+        <div className="w-full max-w-sm bg-gray-800 border border-gray-700 shadow mx-2 my-2 rounded-lg flex flex-col justify-between">
             {children}
         </div>
     )
@@ -37,7 +37,7 @@ const Footer = (props) => {
     const { price } = props
     return(
         <div className="flex items-center justify-between px-5 pb-5">
-            <span className="text-xl font-bold text-white">{price}</span>
+            <span className="text-xl font-bold text-white">Rp {" "} {price.toLocaleString('id-ID', ({styles: 'currency', currency: "IDR"}))}</span>
             <Button variant="bg-blue-600">Add to Cart</Button>
         </div>
     )
